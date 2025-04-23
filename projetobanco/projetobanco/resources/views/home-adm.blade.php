cac29c9cffe1295b3bc1f2c6194a36f80632970b
William Costa <w.williamcosta@gmail.com>
2025-04-22 21:07:46 -0300

    A

--- projetobanco/projetobanco/resources/views/home-adm.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layout')
-@section('principal')
-  <h1>Bem vindo administrador! {{Auth::user()->name}} </h1>
-@endsection
--- projetobanco/projetobanco/resources/views/home-cli.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layout')
-@section('principal')
-  <h1>Bem vindo cliente! {{Auth::user()->name}} </h1>
-@endsection
